60050f3cf24cd1da7ec569fa1faaec1029dd0d6e
FarhanAdiyasa <adiyasaa56@gmail.com>
2026-01-09 16:06:53 +0700

    fix(header): use localized Link for feedback/suggest routes to prevent 404

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
+import { Link } from '@/i18n/routing'
 import { arrayMove } from '@dnd-kit/sortable'
 import { useTranslations } from 'next-intl'
 import Hero from '@/components/Hero'
